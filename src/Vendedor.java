5379bb6c0a69d47a5497cf19bd1ca81e46e9f077
Nia <estefania.betancur1@soy.sena.edu.co>
2024-05-07 08:35:58 -0500

    Herencia y Polimorfismo - Implementación del Método calcularSalario() en Clase Vendedor por Estefania Betancur

--- src/Vendedor.java
+++ src/Vendedor.java
@@ -1,6 +1,10 @@
 public class Vendedor extends Empleado2{
+    private float Comision;
+
     public Vendedor(String nombre, float salarioBasico) {
         super(nombre, salarioBasico);
     }
-
+    public float CalcularSalario(){
+        return super.CalcularSalario() + Comision;
+    }
 }
