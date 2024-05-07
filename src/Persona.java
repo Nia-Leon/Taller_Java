787d7ca8544532d7380c3fd0702c0209213159d1
Nia <estefania.betancur1@soy.sena.edu.co>
2024-05-07 07:26:18 -0500

    Encapsulamiento - Implementación de Métodos para Manipulación de Propiedades por Estefania Betancur

--- src/Persona.java
+++ src/Persona.java
@@ -2,4 +2,34 @@ public class Persona {
     private String Nombre;
     private Integer Edad;
     private Integer FechaNacimiento;
+
+    public Persona(String nombre, Integer edad, Integer fechaNacimiento) {
+        Nombre = nombre;
+        Edad = edad;
+        FechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(Integer edad) {
+        Edad = edad;
+    }
+
+    public Integer getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Integer fechaNacimiento) {
+        FechaNacimiento = fechaNacimiento;
+    }
 }
