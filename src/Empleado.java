4c5b7fbcaf65a56555bb8d3fffdc59df67ed783e
Nia <estefania.betancur1@soy.sena.edu.co>
2024-05-07 07:58:38 -0500

    Polimorfismo - Implementación en Clase Empleado por Estefania Betancur

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,2 +1,9 @@
-public class Empleado {
+public class Empleado extends Persona implements Trabajador {
+    public Empleado(String nombre, Integer edad, Integer fechaNacimiento) {
+        super(nombre, edad, fechaNacimiento);
+    }
+
+    public void realizarTarea(){
+        System.out.println("Realizando tarea escolar");
+    }
 }
