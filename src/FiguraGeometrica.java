08b8299dd1943dbab6eeba2a3b750eead1f0ab0c
Nia <estefania.betancur1@soy.sena.edu.co>
2024-05-07 08:03:22 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Manipulación de Propiedades por Estefania Betancur

--- src/FiguraGeometrica.java
+++ src/FiguraGeometrica.java
@@ -1,4 +1,25 @@
 public class FiguraGeometrica {
     private String NombreFigura;
     private String ColorFigura;
+
+    public FiguraGeometrica(String nombreFigura, String colorFigura) {
+        NombreFigura = nombreFigura;
+        ColorFigura = colorFigura;
+    }
+
+    public String getNombreFigura() {
+        return NombreFigura;
+    }
+
+    public void setNombreFigura(String nombreFigura) {
+        NombreFigura = nombreFigura;
+    }
+
+    public String getColorFigura() {
+        return ColorFigura;
+    }
+
+    public void setColorFigura(String colorFigura) {
+        ColorFigura = colorFigura;
+    }
 }
